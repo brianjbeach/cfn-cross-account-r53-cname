2ff9ddf9ac71b112bb699ac7100d35d57249d5bf
Brian Beach <social@brianbeach.com>
2017-01-07 13:17:12 -0500

    Create and Delete working with SNS
    Create and Delete working with SNS.  Need to add support for Update.
    Also added an input param for account.

--- create-cname.py
+++ create-cname.py
@@ -1,36 +1,58 @@
-import boto3, json, urllib, urllib2, json
+import sys, boto3, json, urllib, urllib2, json
 client = boto3.client('route53')
     
 def lambda_handler(event, context):
+    #SNS events contain a wrapper around the Lambda event. Unpack the
+    #lambda event from SNS. Delete this part if calling lambda directly.
+    print("SNS Event: " + json.dumps(event))
+    event = json.loads(event['Records'][0]['Sns']['Message'])
+    print("Lambda Event: " + json.dumps(event))
+    processCustomResource(event, context)
     
-    hostedzone = event['ResourceProperties']['HostedZoneId']
-    source = event['ResourceProperties']['Name']
-    target = event['ResourceProperties']['Value']
-  
-    try:               
-        response = client.change_resource_record_sets(
-    		HostedZoneId=hostedzone,
-    		ChangeBatch= {
-    			'Comment': 'add %s -> %s' % (source, target),
-    			'Changes': [{
-    				'Action': 'UPSERT',
-    				'ResourceRecordSet': {
-    					'Name': source,
-    					'Type': 'CNAME',
-    					'TTL': 300,
-    					'ResourceRecords': [{'Value': target}]
-    				}
-    			}]
-    		}
-    	)
+    
+def processCustomResource(event, context): 
+    try: 
+        type = event['RequestType']
+        hostedzone = event['ResourceProperties']['HostedZoneId']
+        source = event['ResourceProperties']['Name']
+        target = event['ResourceProperties']['Value']
+        
+        if type == 'Delete':
+            action = "DELETE"
+            print "Deleting CNAME " + source + "->" + target + " in " + hostedzone
+        else:
+            action = "UPSERT"
+            print "Creating CNAME " + source + "->" + target + " in " + hostedzone
+        change_resource_record_sets(action, hostedzone, source, target)
+        
+        print "Completed successfully"
         responseStatus = 'SUCCESS'
         responseData = {}
         sendResponse(event, context, responseStatus, responseData)
+        
     except: 
+        print("Error:", sys.exc_info()[0])
         responseStatus = 'FAILED'
-        responseData = {'Error': 'Something bad happened'}
+        responseData = {'Error': ("Unexpected error: ", sys.exc_info()[0])}
         sendResponse(event, context, responseStatus, responseData)
-	
+
+
+def change_resource_record_sets(action, hostedzone, source, target):
+    response = client.change_resource_record_sets(
+		HostedZoneId=hostedzone,
+		ChangeBatch= {
+			'Comment': 'CNAME %s -> %s' % (source, target),
+			'Changes': [{
+				'Action': action,
+				'ResourceRecordSet': {
+					'Name': source,
+					'Type': 'CNAME',
+					'TTL': 300,
+					'ResourceRecords': [{'Value': target}]
+				}
+			}]
+		}
+	)
 
 def sendResponse(event, context, responseStatus, responseData):
     data = json.dumps({
